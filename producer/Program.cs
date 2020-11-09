0e580c82f05f8693a7de261396ee7835705b1a54
Thales Pilão <thalespilao@hotmail.com>
2020-11-09 21:52:28 +0000

    kafka producer v1

--- producer/Program.cs
+++ producer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,85 +16,38 @@ namespace producer
     {
         static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            int interval = 1000;
+            bool parsed = false;
+            if(args.Any())
+                parsed = Int32.TryParse(args[0], out interval);
 
-        private static IHostBuilder CreateHostBuilder(string[] args) => 
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((context, collection) => {
-                    // collection.AddHostedService<KafkaConsumerHostedService>();
-                    collection.AddHostedService<KafkaProducerHostedService>();
-                });
-    }
-
-    public class KafkaProducerHostedService : IHostedService
-    {
-        private readonly ILogger<KafkaProducerHostedService> _logger;
-        private IProducer<Null, string> _producer;
-
-        public KafkaProducerHostedService(ILogger<KafkaProducerHostedService> logger)
-        {
-            this._logger = logger;
-            var config = new ProducerConfig(){
+            var conf = new ProducerConfig
+            {
                 BootstrapServers = "localhost:9092"
             };
-            this._producer = new ProducerBuilder<Null, string>(config).Build();
-        }
 
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            for(var i = 0; i < 10; i++)
+            var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
             {
-                var messageToSend = $"Hello World, we're on message {i}";
-                this._logger.LogInformation(messageToSend);
-                await this._producer.ProduceAsync("demo", new Message<Null, string>(){
-                    Value = messageToSend
-                }, cancellationToken);
+                Console.WriteLine("we just captured the process exit."); // <-- this is never reached.
+                e.Cancel = true;
+                cts.Cancel();
+            };
+
+            using var producer = new ProducerBuilder<Null, string>(conf).Build();
+
+            int counter = 0;
+            while (!cts.IsCancellationRequested)
+            {
+                counter++;
+
+                producer.Produce("demo", new Message<Null, string> { Value = $"Message{counter}" }, (message) =>
+                {
+                    Console.WriteLine($"Message with content {message.Value} sended successfully");
+                });
+    
+                Thread.Sleep(TimeSpan.FromMilliseconds(parsed ? interval : 1000));
             }
-
-            _producer.Flush();
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            this._producer?.Dispose();
-            Console.WriteLine($"Producer on class {nameof(KafkaProducerHostedService)} disposed.");
-            return Task.CompletedTask;
-        }
-    }
-
-    public class KafkaConsumerHostedService : IHostedService
-    {
-        private readonly ILogger<KafkaConsumerHostedService> _logger;
-        private readonly ClusterClient _cluster;
-
-        public KafkaConsumerHostedService(ILogger<KafkaConsumerHostedService> logger)
-        {
-            this._logger = logger;
-            this._cluster = new ClusterClient(new Configuration(){
-                Seeds = "localhost:9092"
-            }, new ConsoleLogger());
-        }
-
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            _cluster.ConsumeFromLatest(topic: "demo");
-            _cluster.MessageReceived += record => 
-            {
-                _logger.LogInformation($"Received: {Encoding.UTF8.GetString(record.Value as byte[])}");
-            };
-            _cluster.MessageDiscarded += record => 
-            {
-                _logger.LogInformation($"Discarded: {Encoding.UTF8.GetString(record.Value as byte[])}");
-            };
-            return Task.CompletedTask;
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            this._cluster?.Dispose();
-            Console.WriteLine($"Producer on class {nameof(KafkaConsumerHostedService)} disposed.");
-            return Task.CompletedTask;
         }
     }
 }
